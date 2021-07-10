7792f097e987c98b30fbcd6b7bcde5d24167643d
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-07-10 13:46:42 +0900

    Update stanの基本.R

--- stanの基本.R
+++ stanの基本.R
@@ -1,51 +1,55 @@
-#stan$B%U%!%$%k$N4pK\;v9`$HJ,@O$N4pK\(B
-
+#stanの基本
+#stanの基本事項とR,stanの連携分析
+#目的:stanを用いた分析を図式化
 
 install.packages('rstan',repos='http://cloud.r-project.org/',dependencies = TRUE)
 
 library(rstan)
 
-#$B7W;;$N9bB.2=(B
+#計算の高速化
 rstan_options(auto_write=TRUE)
 options(mc.cores=parallel::detectCores())
 
-#$BJ,@OBP=h$N%G!<%?(B
+#分析対象のデータ
 file_beer_sales_1<-read.csv("2-4-1-beer-sales-1.csv")
 head(2-4-1-beer-sales-1.csv,n=3)
 
-#$B%G!<%?$N3NG'(B
+#分析対象のデータの最初から3までの数値
 head(file_beer_sales_1,n=3)
 
-#$B%5%s%W%k%5%$%:(B
+#サンプルのサイズ
 sample_size<-nrow(file_beer_sales_1)
 sample_size
 
-#list$B$K$^$H$a$k(B
+#listにまとめる
 data_list<-list(sales=file_beer_sales_1$sales, N=sample_size)
 data_list
 
-#mcmc$B$N<B9T(B
+#mcmcの実行
 mcmc_result<-stan(
-  file="2-4-1-calc-mean-variance.stan", #stan$B%U%!%$%k(B
-  data=data_list, #$BBP>]%G!<%?(B
-  seed=1, #$BMp?t$N<o(B
-  chains=4, #$B%A%'!<%s?t(B
-  iter=2000, #$BMp?t@8@.$N7+$jJV$7?t(B
-  warmup=1000, #$B%P!<%s%$%s4|4V(B
-  thin=1 #$B4V0z$-?t(B(1$B$J$i4V0z$-L5$7(B)
+  file="2-4-1-calc-mean-variance.stan", #stanファイル
+  data=data_list, #対象のデータ
+  seed=1, #乱数の種
+  chains=4, #チェーン数
+  iter=2000, #乱数生成の反復数
+  warmup=1000, #バーンイン期間
+  thin=1 #間引き数(1なら間引き無し)
 )
 
-#$B7k2L$NI=<((B
+#結果の表示
 print(
-  mcmc_result, #MCMC$B%5%s%W%j%s%0$N7k2L(B
-  probs=c(0.025,0.5,0.975) #$BCf1{CM$H(B95%$B?.MQ6h4V$r=PNO(B
+  mcmc_result, #MCMCサンプリングの結果
+  probs=c(0.025,0.5,0.975) #中央値と95%信用区間を出力
 )
 
-#$B%H%l!<%9%W%m%C%H(B($B%P!<%s%$%s4|4V$J$7(B)
+#トレースプロット(バーンイン期間無し)
 traceplot(mcmc_result)
 
-#$B%H%l!<%9%W%m%C%H4|4V(B
+#トレースプロット(バーンイン期間あり)
 traceplot(mcmc_result,inc_warmup=T)
 
-
-
+/
+#サンプル:標本
+#MCMCサンプリング:mcmcを用いて得られた乱数
+#mcmc:マルコフ連鎖モンテカルロ法:求める確率分布をマルコフ連鎖を作成することでサンプリングを行うアルゴリズム
+#バーンイン期間ありなし:mcmcサンプルの初期値(分布)が安定しないため収束させ,mcmcサンプルを用いる
