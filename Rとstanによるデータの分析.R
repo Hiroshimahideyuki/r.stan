e6f17e85d3d8640bad12b3f0787df7bb278eb650
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-07-13 13:00:21 +0900

    Update Rとstanによるデータの分析.R

--- Rとstanによるデータの分析.R
+++ Rとstanによるデータの分析.R
@@ -1,14 +1,19 @@
+#Stanの実装の方法について
 
+#分析対象のデータの読み込み
 file_beer_sales_ab<-read.csv("2-6-1-beer-sales-ab.csv")
 head(file_beer_sales_ab, n=3)
 
+#ビールの種類別のヒストグラム
 library("ggplot2")
 ggplot(data=file_beer_sales_ab,
        mapping=aes(x=sales,y=..density..,
                    color=beer_name,fill=beer_name))+
   geom_histogram(alpha=0.5,position="identity")+
   geom_density(alpha=0.5,size=0)
+#2種類のグラフの重ね合わせ
 
+#ビールの種類別にデータを分ける
 sales_a<-file_beer_sales_ab$sales[1:100]
 sales_b<-file_beer_sales_ab$sales[101:200]
 
@@ -32,5 +37,7 @@ print(
 
 library(ggplot2)
 library(bayesplot)
+
+#平均値の差の事後分布のグラフ
 mcmc_sample<-rstan::extract(mcmc_result_6,permuted=FALSE)
 mcmc_dens(mcmc_sample,pars="diff")
