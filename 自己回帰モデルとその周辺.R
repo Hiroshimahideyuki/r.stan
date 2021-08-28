9a2707edd34439ddb07e311bfca36b5ec2460a48
Hiroshimahideyuki <84165155+Hiroshimahideyuki@users.noreply.github.com>
2021-08-29 06:57:13 +0900

    Update 自己回帰モデルとその周辺.R

--- 自己回帰モデルとその周辺.R
+++ 自己回帰モデルとその周辺.R
@@ -1,24 +1,26 @@
+#時系列モデルの一種である自己回帰モデルの説明について
 
-
+#パッケージの読み込み
 library(rstan)
 library(bayesplot)
 library(ggfortify)
-
+#計算の高速化
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
+#データの読み込み
 sales_df_5 <- read.csv("5-7-1-sales-ts-5.csv")
 sales_df_5$date <- as.POSIXct(sales_df_5$date)
 head(sales_df_5, n = 3)
-
+#図示
 autoplot(ts(sales_df_5[, -1]))
 
-
+#データの準備
 data_list <- list(
   y = sales_df_5$sales, 
   T = nrow(sales_df_5)
 )
-
+#自己回帰モデルの推定
 autoregressive <- stan(
   file = "5-7-1-autoregressive.stan",
   data = data_list,
@@ -26,6 +28,7 @@ autoregressive <- stan(
   control = list(max_treedepth = 15)
 )
 
+#自己モデルの推定結果
 print(autoregressive, 
       par = c("s_w", "b_ar", "Intercept", "lp__"),
       probs = c(0.025, 0.5, 0.975))
